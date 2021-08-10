830a9532e01487092013b426be21b41b067d6c1a
Caroline Sun <carolinesun@carolines-mbp.lan>
2021-08-10 16:49:44 -0400

    personal profile webstie with home page, resume, projects, etc

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//import libraries
+import React from 'react';
+import './index.css';
+import App from './App';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
